4c00e913ad51dcba554e961198362a0d43426cfa
Ivan Yurchenko <ivan.yurchenko.ua@gmail.com>
2018-02-26 13:25:26 +0200

    Implemented the test task

---
+++ UpworkTestQuestion/Operation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UpworkTestQuestion
+{
+    static class Operation
+    {
+
+        public static Func<int, bool> IsOdd()
+        {
+            return x => x % 2 != 0;
+        }
+
+        public static Func<int, bool> IsPrime()
+        {
+            return x =>
+            {
+                if (x == 1) return false;
+                if (x == 2) return true;
+                if (x % 2 == 0) return false;
+
+                var boundary = (int)Math.Floor(Math.Sqrt(x));
+
+                for (int i = 3; i <= boundary; i += 2)
+                {
+                    if (x % i == 0) return false;
+                }
+
+                return true;
+            };
+        }
+
+        public static Func<int, bool> IsPalindrome()
+        {
+            return x =>
+            {
+                var s = x.ToString();
+                for (int i = 0; i < s.Length / 2; i++)
+                {
+                    if (s[i] != s[s.Length - i - 1]) return false;
+                }
+
+                return true;
+            };
+        }
+    }
+}
--- UpworkTestQuestion/Program.cs
+++ UpworkTestQuestion/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace UpworkTestQuestion
 {
@@ -6,7 +8,41 @@ namespace UpworkTestQuestion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var amount = int.Parse(Console.ReadLine());
+
+            var operations = new Dictionary<int, Func<int, bool>>();
+            operations.Add(1, Operation.IsOdd());
+            operations.Add(2, Operation.IsPrime());
+            operations.Add(3, Operation.IsPalindrome());
+
+            for (int i = 0; i < amount; i++)
+            {
+                var line = Console.ReadLine();
+                var conditionNumber = int.Parse(line.Split(' ')[0]);
+                var number = int.Parse(line.Split(' ')[1]);
+                var result = operations[conditionNumber](number);
+
+                string output;
+                switch (conditionNumber)
+                {
+                    case 1:
+                        output = result ? "ODD" : "EVEN";
+                        break;
+                    case 2:
+                        output = result ? "PRIME" : "COMPOSITE";
+                        break;
+                    case 3:
+                        output = result ? "PALINDROME" : "NOT PALINDROME";
+                        break;
+                    default:
+                        output = "WRONG OPERATION";
+                        break;
+                }
+
+                Console.WriteLine(output);
+            }
+
+            Console.ReadLine();
         }
     }
 }
